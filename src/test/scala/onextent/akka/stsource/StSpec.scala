c52e7bd0911f3b9b1743897f6a291bbbf07a359f
Ed Sweeney <ed@onextent.com>
2018-11-03 22:49:08 -0700

    first tests for st

--- src/test/scala/onextent/akka/stsource/StSpec.scala
+++ src/test/scala/onextent/akka/stsource/StSpec.scala
@@ -1,12 +1,35 @@
 package onextent.akka.stsource
 
+import java.net.URL
+
 import com.typesafe.scalalogging.LazyLogging
 import org.scalatest._
+import org.stringtemplate.v4.{ST, STGroupFile}
 
 class StSpec extends FlatSpec with Matchers with LazyLogging {
 
-  ignore should "read all" in {
+  "hello" should "world" in {
 
+    val hello = new ST("Hello, <name>")
+
+    hello.add("name", "World")
+
+    assert("Hello, World" == hello.render())
+
+  }
+
+  "group" should "file" in {
+
+    val gfile: URL = getClass.getResource("/test.stg")
+    val group = new STGroupFile(gfile, "UTF8", '<', '>')
+
+    val st = group.getInstanceOf("decl")
+    st.add("type", "int")
+    st.add("name", "x")
+    st.add("value", 0)
+    val result = st.render
+
+    assert("int x = 0;" == result)
 
   }
 
